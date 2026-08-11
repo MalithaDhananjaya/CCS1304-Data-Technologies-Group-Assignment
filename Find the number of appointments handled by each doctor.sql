6312b1b8429f36731b7e8e0647c263415491d3f0
Manula Gunawardhana <manula2481@gmail.com>
2026-08-11 22:20:47 +0530

    Delete Find the number of appointments handled by each doctor.sql

--- Find the number of appointments handled by each doctor.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    d.doctor_id,
-    d.doctor_name,
-    d.specialization,
-    COUNT(a.appointment_id) AS total_appointments
-FROM Doctor d
-LEFT JOIN Appointment a ON d.doctor_id = a.doctor_id
-GROUP BY d.doctor_id, d.doctor_name, d.specialization;
