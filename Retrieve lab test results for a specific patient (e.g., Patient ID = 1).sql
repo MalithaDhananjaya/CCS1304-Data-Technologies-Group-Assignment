70913ecf0e0cd44014707a0579ab05313ae58dfc
Manula Gunawardhana <manula2481@gmail.com>
2026-08-11 22:20:02 +0530

    Delete Retrieve lab test results for a specific patient (e.g., Patient ID = 1).sql

--- Retrieve lab test results for a specific patient (e.g., Patient ID = 1).sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    p.full_name AS patient_name,
-    lt.test_name,
-    lt.test_date,
-    lt.test_result,
-    lt.test_status
-FROM Lab_Test lt
-JOIN Patient p ON lt.patient_id = p.patient_id
-WHERE p.patient_id = 1;
