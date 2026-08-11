592aa4de32f8a6ff445e37a6723d9edf62326573
Manula Gunawardhana <manula2481@gmail.com>
2026-08-11 22:33:52 +0530

    Update sp_AdmitPatient.sql

--- Procedure/sp_AdmitPatient.sql
+++ Procedure/sp_AdmitPatient.sql
@@ -5,11 +5,11 @@ CREATE PROCEDURE sp_AdmitPatient(
     IN p_room_id INT
 )
 BEGIN
-    -- 1. Admission එක Insert කිරීම
+    -- 1. Insert Admission
     INSERT INTO Admission (patient_id, room_id, admission_date, admission_status)
     VALUES (p_patient_id, p_room_id, NOW(), 'Admitted');
     
-    -- 2. Room එක 'Occupied' ලෙස Update කිරීම
+    -- 2. Room update as 'Occupied'
     UPDATE Room 
     SET room_status = 'Occupied' 
     WHERE room_id = p_room_id;
