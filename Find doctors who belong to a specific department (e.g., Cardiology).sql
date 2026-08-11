af3601c54c1191b947148eb78f1b0d5e2f81c8d1
Manula Gunawardhana <manula2481@gmail.com>
2026-08-11 22:20:37 +0530

    Delete Find doctors who belong to a specific department (e.g., Cardiology).sql

--- Find doctors who belong to a specific department (e.g., Cardiology).sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    d.doctor_name,
-    d.qualification,
-    d.consultation_fee,
-    dept.dept_name
-FROM Doctor d
-JOIN Department dept ON d.department_id = dept.department_id
-WHERE dept.dept_name = 'Cardiology';
